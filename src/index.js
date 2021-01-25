368012d2fa1b6a174d545198adf25924fb552932
Kevin Bastoul <kaycbas@gmail.com>
2021-01-25 12:29:55 -0800

    reorganize

--- src/index.js
+++ src/index.js
@@ -2,18 +2,14 @@ const {
   daysOfYear
 } = require('./util');
 
-// USE THESE DOCS: https://api.anychart.com/anychart.charts.Map
-
 anychart.onDocumentReady(() => {
 
   // create map
   let map = anychart.map();
   map.removeAllListeners();
-  // map.title('2020 in Search')
 
   let allData = require('../assets/data/trends_2020.json');
   let states = allData["2020-1-1"];
-  // let states = require('../temp.json');
 
   // create data set
   let dataSet = anychart.data.set(states);
@@ -24,37 +20,71 @@ anychart.onDocumentReady(() => {
   // enable labels
   series.labels(true);
   labels = series.labels();
-    
-  // labels setting
+
+  // configure label settings
   labels.fontColor('white');
   labels.fontSize("14px");
   labels.fontFamily('Roboto');
   labels.fontWeight('bold');
   labels.offsetY(-10);
-  labels.connectorStroke({ color: '#DCDCDC' , thickness: 2 });
+  labels.connectorStroke({
+    color: '#DCDCDC',
+    thickness: 2
+  });
   labels.padding(6);
   labels.hAlign('center')
-
   map.overlapMode(true);
-  // map.overlapMode("no-overlap");
 
   series.geoIdField('id');
 
-  map.background().fill('#1b262c')
-
-  series.stroke("#1b262c");
-
   // set map color settings
-  series.colorScale(anychart.scales.ordinalColor([
-    {from:0, to:9, color: '#F94144'},
-    {from:10, to:19, color: '#F3722C'},
-    {from:20, to:29, color: '#F7B926'},
-    {from:30, to:39, color: '#90BE6D'},
-    {from:40, to:49, color: '#43AA8B'},
-    {from:50, to:59, color: '#4D908E'},
-    {from:60, to:69, color: '#577590'},
-    {from:70, to:70, color: '#277DA1'},
-    {from:80, to:90, color: '#8F43AB'}
+  map.background().fill('#1b262c')
+  series.stroke("#1b262c");
+  series.colorScale(anychart.scales.ordinalColor([{
+      from: 0,
+      to: 9,
+      color: '#F94144'
+    },
+    {
+      from: 10,
+      to: 19,
+      color: '#F3722C'
+    },
+    {
+      from: 20,
+      to: 29,
+      color: '#F7B926'
+    },
+    {
+      from: 30,
+      to: 39,
+      color: '#90BE6D'
+    },
+    {
+      from: 40,
+      to: 49,
+      color: '#43AA8B'
+    },
+    {
+      from: 50,
+      to: 59,
+      color: '#4D908E'
+    },
+    {
+      from: 60,
+      to: 69,
+      color: '#577590'
+    },
+    {
+      from: 70,
+      to: 70,
+      color: '#277DA1'
+    },
+    {
+      from: 80,
+      to: 90,
+      color: '#8F43AB'
+    }
   ]));
   series.hovered().fill('#577590');
 
@@ -68,10 +98,13 @@ anychart.onDocumentReady(() => {
 });
 
 const getMonthDayStr = (date) => {
-  let month = date.toLocaleString('default', { month: 'short' });
+  let month = date.toLocaleString('default', {
+    month: 'short'
+  });
   let day = date.getUTCDate();
   return month.concat(' ', day, ', ');
 }
+
 const addEventListeners = (allData, dataSet) => {
   let slider = document.querySelector(".rs-range");
   let playButton = document.querySelector('.play-button');
@@ -83,19 +116,13 @@ const addEventListeners = (allData, dataSet) => {
   let sources = document.querySelector('.sources');
   let monthDayLabel = document.querySelector('.month-day');
   let yearLabel = document.querySelector('.year');
+
   let date = new Date(daysOfYear[0].replace(/-/g, "/"));
+
   let playing = false;
-  
+
   monthDayLabel.innerText = getMonthDayStr(date);
   yearLabel.innerText = date.getFullYear();
-
-  const updateState = () => {
-    dataSet.data(allData[daysOfYear[slider.value]])
-
-    let date = new Date(daysOfYear[slider.value].replace(/-/g, "/"));
-    monthDayLabel.innerText = getMonthDayStr(date); 
-    yearLabel.innerText = date.getFullYear();
-  }
 
   infoButton.addEventListener('click', () => {
     if (infoModal.style.display === "block") {
@@ -123,7 +150,7 @@ const addEventListeners = (allData, dataSet) => {
   }
 
   slider.onchange = () => {
-    const timestamp = Math.floor((slider.value/365)*182)
+    const timestamp = Math.floor((slider.value / 365) * 182)
     document.querySelector('#audio').currentTime = timestamp;
   }
 
@@ -153,9 +180,16 @@ const addEventListeners = (allData, dataSet) => {
     }
   })
 
+  const updateState = () => {
+    dataSet.data(allData[daysOfYear[slider.value]])
+    let date = new Date(daysOfYear[slider.value].replace(/-/g, "/"));
+    monthDayLabel.innerText = getMonthDayStr(date);
+    yearLabel.innerText = date.getFullYear();
+  }
+
   volumeButton.addEventListener('click', () => {
     let audio = document.querySelector('#audio');
-    
+
     if (volumeButton.classList.contains('muted')) {
       volumeButton.classList.remove('muted');
       audio.muted = false;
@@ -164,5 +198,4 @@ const addEventListeners = (allData, dataSet) => {
       audio.muted = true;
     }
   })
-
 }
